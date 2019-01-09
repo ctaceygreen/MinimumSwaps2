c39b62267345018ba5b7e65c76830cccc724228a
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:52:43 +0000

    Add project files.

---
+++ ArrayManipulation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries)
+    {
+        long[] array = new long[n];
+        for(long queryRow = 0; queryRow < queries.GetLength(0); queryRow++)
+        {
+            var query = queries[queryRow];
+            int startIndex = query[0];
+            int endIndex = query[1];
+            long increment = query[2];
+            for(int index = startIndex - 1; index < endIndex; index++)
+            {
+                array[index] += increment;
+            }
+        }
+        long max = long.MinValue;
+        for(int i = 0; i < array.Length; i++)
+        {
+            max = Math.Max(max, array[i]);
+        }
+        return max;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nm = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nm[0]);
+
+        int m = Convert.ToInt32(nm[1]);
+
+        int[][] queries = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ MinimumSwaps2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr)
+    {
+        var swapSum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            while (arr[i] != i + 1)
+            {
+                var swapKey = arr[i] - 1;
+
+                var temp = arr[i];
+
+                arr[i] = arr[swapKey];
+                arr[swapKey] = temp;
+
+                swapSum++;
+            }
+        }
+
+        return swapSum;
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
